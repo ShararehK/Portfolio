8cf2fa7a8ae4081f7df61c2b53302687ff3ab490
kessha01 <kessha01@dolembreux.ca>
2023-03-15 00:30:23 -0400

    change background

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-handleSubmit(event) {
-  event.preventDefault();
-  const { name, email, message } = this.state;
-  // save message in data.json
-  const data = {
-    name,
-    email,
-    message
-  };
-  axios.post('/data', data)
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));
-  // show message popup
-  alert("Thanks a lot for reaching me out. I will contact you as soon as possible.");
-  // clear form
-  this.setState({
-    name: '',
-    email: '',
-    message: ''
-  });
-}
+// handleSubmit(event) {
+//   event.preventDefault();
+//   const { name, email, message } = this.state;
+//   // save message in data.json
+//   const data = {
+//     name,
+//     email,
+//     message
+//   };
+//   axios.post('/data', data)
+//     .then(res => console.log(res.data))
+//     .catch(err => console.log(err));
+//   // show message popup
+//   alert("Thanks a lot for reaching me out. I will contact you as soon as possible.");
+//   // clear form
+//   this.setState({
+//     name: '',
+//     email: '',
+//     message: ''
+//   });
+// }
